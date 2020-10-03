0036c9681e39f43ae4fb125bf4265a53dd14eaa4
abhikeshri10 <60318491+abhikeshri10@users.noreply.github.com>
2020-10-03 20:13:18 +0530

    added files commit 1

---
+++ src/Server/Server.java
@@ -0,0 +1,41 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.sql.*;
+import java.sql.SQLException;
+
+public class Server {
+
+    public static void main(String args[]){
+       Database.getConnection();
+        ServerSocket serverSocket;
+        Socket socket;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Server started");
+        try {
+            serverSocket = new ServerSocket(5436);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+         while (true) {
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Client Connected");
+                Thread t = new Thread(new HandleQuery(socket));//listen message from client
+                t.start();
+               
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+                
+            }
+        
+    }
+
+}
