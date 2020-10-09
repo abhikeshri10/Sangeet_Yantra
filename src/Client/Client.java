35962cbd3dfc2b9ad321e71ecc3dd81c740fe463
abhikeshri10 <60318491+abhikeshri10@users.noreply.github.com>
2020-10-09 19:04:20 +0530

    added response class

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,16 +1,30 @@
 package Client;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Client {
+public class Client  extends Application {
     public static Socket socket = null;
     public static ObjectOutputStream objectOutputStream =null;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        primaryStage.setTitle("Sangeet Yantra");
+        primaryStage.setScene(new Scene(root, 800, 625));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-
+            //launch(args);
         try {
              socket = new Socket("localhost", 5436);
 
@@ -44,6 +58,8 @@ public class Client {
 
             // Thread t = new Thread(new HandleClient(socket));
             // t.start();
+           // launch(args);
+
         } catch (Exception e) {
 
             e.printStackTrace();
